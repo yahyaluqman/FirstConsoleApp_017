970571edb5b2b214d39076ae01883d3f152707b7
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-10-10 19:41:38 +0700

    Add project files.

---
+++ FirstConsoleApp_017/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp_017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
